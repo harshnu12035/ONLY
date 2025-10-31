9b813eaa1ba0f65d59db79b28c711517d1eb721b
𝐒𝐏𝐀𝐌 𝐁𝐎𝐓 𝐂𝐎𝐃𝐄𝐑 🚩 <light12035@gmail.com>
2025-10-31 17:30:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -78,7 +78,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://files.catbox.moe/sz8xwz.jpg"
+    "START_IMG_URL", "https://files.catbox.moe/8ep6yo.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://files.catbox.moe/9cevdg.jpg"
